e2759f8719dbe5b1cc16bad86340f765d1c91bf3
ADL\Fitri_117820 <fitri.yadi@axiatadigitallabs.com>
2023-09-02 20:23:01 +0700

    fix: sql: Scan error on column index 1, name "created_at": unsupported Scan, storing driver.Value type []uint8 into type *time.Time

--- db/dbGorm.go
+++ db/dbGorm.go
@@ -13,14 +13,11 @@ var dbGorm *gorm.DB
 func InitGorm() {
 	var err error
 
-	// dsn := "gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"
-	dbinfo := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	dbinfo := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	dbGorm, err = gorm.Open(mysql.Open(dbinfo), &gorm.Config{})
 	if err != nil {
 		panic("failed Gorm to connect database")
 	}
-
-	// defer dbGorm.Statement.ReflectValue.Close()
 
 }
 
